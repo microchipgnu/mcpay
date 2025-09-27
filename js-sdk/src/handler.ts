0082d01e1ed33ee94e79efb8f35648845542ec8d
Luís Freitas <microchipgnu@gmail.com>
2025-09-27 15:29:43 +0530

    Bump SDK version to 0.1.4 and update handler exports
    - Updated package.json to reflect version change from 0.1.3 to 0.1.4.
    - Enhanced handler.ts by adding new exports for createMcpHandler and additional plugin functions, improving modularity and functionality.

--- js-sdk/src/handler.ts
+++ js-sdk/src/handler.ts
@@ -1,2 +1,6 @@
+import createMcpHandler from "./handler/server";
+
 export { createMcpPaidHandler } from "./handler/server/templates/x402-server";
 export { withX402 } from "./handler/server/plugins/with-x402";
+export { makePlugins, composePlugins } from "./handler/server";
+export { createMcpHandler };
